1aa7a7ff8f2107677c9125d53d53b28dd50fc5f4
Georg Herison <georgherison@gmail.com>
2024-10-18 22:01:17 -0300

    started

---
+++ src/components/LoginComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="flex flex-col items-center justify-center bg-gray-100 rounded-xl w-[450px] h-[500px] shadow-md ">
+        <img src="../assets/teste 1.svg" class="w-[300px] h-[200px]">
+        <form @submit.prevent=""   >
+            <div class="flex-col flex m-5 w-[300px]">
+                <label class="flex items-start">Email</label>
+                <input class="  border-b-[1px] border-black bg-transparent outline-none">
+            </div>
+            <div class="flex-col flex m-5  ">
+                <label class="flex items-start">Password</label>
+                <input type="password" class="  border-b-[1px] border-black bg-transparent outline-none">
+                
+            </div>
+
+            <button class="items-center justify-center w-[300px] h-[50px] bg-red-600 rounded-md font-bold text-white mt-2">Sign in</button>
+           
+            
+        </form>
+        <div class="flex items-center mt-5">
+                <h3>Don't have an account yet?</h3>
+                <h3 @click="showSignUp = true" class="ml-2 text-blue-500 underline cursor-pointer">Sign up</h3>
+            </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginComponent',
+};
+</script>
---
+++ src/views/HomeScreen.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import LoginComponent from '@/components/LoginComponent.vue';
+</script>
+
+<template>
+    <div class="flex justify-center items-center min-h-screen">
+        <LoginComponent></LoginComponent>
+    </div>
+    
+</template>
+
